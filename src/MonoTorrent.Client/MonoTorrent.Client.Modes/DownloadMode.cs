f4ebfc76752f2a72ad498edf2e7ed690b98ed4ed
kaedei <kaedei@qq.com>
2026-04-27 15:43:55 +0800

    feat: Refresh file lengths when download is complete
    - Added a check to refresh all files' correct lengths when the download is fully complete.

--- src/MonoTorrent.Client/MonoTorrent.Client.Modes/DownloadMode.cs
+++ src/MonoTorrent.Client/MonoTorrent.Client.Modes/DownloadMode.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -90,6 +90,10 @@ namespace MonoTorrent.Client.Modes
         internal async Task UpdateSeedingDownloadingState ()
         {
             //If download is fully complete, set state to 'Seeding' and send an announce to the tracker.
+            if (Manager.Bitfield.AllTrue) {
+                await Manager.RefreshAllFilesCorrectLengthAsync ();
+            }
+
             if (Manager.Complete && state == TorrentState.Downloading) {
                 state = TorrentState.Seeding;
                 await Task.WhenAll (
